3105fe79977460c57c433cb960e91783d47626ed
fuzzzzzz <fuzzzzzz@gmail.com>
2016-10-08 15:30:00 +0200

    few fixes
    recompiling the game shader with the inc files.
    recompile withou env_dof_controller ( client+server)

--- sp/src/materialsystem/stdshaders/buildjurassicshaders.bat
+++ sp/src/materialsystem/stdshaders/buildjurassicshaders.bat
@@ -12,7 +12,7 @@ set SDKBINDIR=G:\Steam\SteamApps\common\Source SDK Base 2013 Singleplayer\bin
 
 rem ==  Set the Path to your mod's root source code ==
 rem This should already be correct, accepts relative paths only!
-set SOURCEDIR=G:\programming\jurassic-life\sp\src
+set SOURCEDIR=G:\programming\jurassic-life-2013\jurassic-life\sp\src
 
 rem ==== MOD PATH CONFIGURATIONS END ===
 rem ====================================
